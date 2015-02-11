e5a720ec2c282d45d42e04141ec61955ae3c03f9
Thiago Oliveira <thiago_pinheiro@atlantico.com.br>
2015-02-11 14:53:36 -0300

    first commit

---
+++ grails-app/init/tasklist/Application.groovy
@@ -0,0 +1,10 @@
+package tasklist
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application)
+    }
+}
